4e0588dc4c10c0cbc6bd4c58b398dca6a07a0991
dhanyathav1 <dhanyatha.angai@gmail.com>
2024-11-08 16:38:27 -0800

    Update AdenArmBm.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ButtonMaps/Arm/AdenArmBm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ButtonMaps/Arm/AdenArmBm.java
@@ -21,25 +21,34 @@ public class ArianaArmBm extends AbstractButtonMap {
         if (opMode.gamepad2.left_trigger > 0.1) {
             if (robot.linearSlidesMotor1.getCurrentPosition() < -5 || robot.linearSlidesMotor2.getCurrentPosition() < -5) {
                 opMode.telemetry.addData("LS Direction", "INHIBIT DOWN");
-                robot.linearSlidesMotor1.setPower(0);
-                robot.linearSlidesMotor2.setPower(0);
+                robot.bucketMotor1.setPower(0);
+                robot.bucketMotor2.setPower(0);
             } else {
                 opMode.telemetry.addData("LS Direction", "DOWN");
-                robot.linearSlidesMotor1.setPower(-linearSlidesDownMultiplier * opMode.gamepad2.left_trigger);
-                robot.linearSlidesMotor2.setPower(-linearSlidesDownMultiplier * opMode.gamepad2.left_trigger);
+                robot.bucketMotor1.setPower(opMode.gamepad2.left_trigger);
+                robot.bucketMotor2.setPower(opMode.gamepad2.left_trigger);
             }
         } else if (opMode.gamepad2.right_trigger > 0.1) {
             opMode.telemetry.addData("LS Direction", "UP");
-            robot.linearSlidesMotor1.setPower(linearSlidesUpMultiplier * opMode.gamepad2.right_trigger);
-            robot.linearSlidesMotor2.setPower(linearSlidesUpMultiplier * opMode.gamepad2.right_trigger);
+            robot.bucketMotor1.setPower(opMode.gamepad2.right_trigger);
+            robot.bucketMotor2.setPower(opMode.gamepad2.right_trigger);
         } else {
             opMode.telemetry.addData("LS Direction", "OFF");
-            robot.linearSlidesMotor1.setPower(0);
-            robot.linearSlidesMotor2.setPower(0);
+            robot.bucketMotor1.setPower(0);
+            robot.bucketMotor2.setPower(0);
         }
 
         if (opMode.gamepad2.y) {
-            robot.airplaneServo.setPosition(0);
+            robot.brushServo1.setPosition(0);
         }
+        if (opMode.gampad2.b) {
+            robot.linearSlidesMotor.setPower(opMode.gamepad2.b);
+        } else {
+            robot.linearSlidesMotor.setPower(0);
+        }
+        if (opMode.gampad2.left_bumper) {
+            robot.clawServo.setPower(opMode.gamepad2.left_bumper);
+        } else { 
+            robot.clawServo.setPower(0);
     }
 }
